59132dc13ef6bcbd1ca804e1d05671434fd83f22
WALLACE NUNES DOS SANTOS <wallace.nsantos1@senacsp.edu.br>
2024-04-03 15:23:33 -0300

    aula 03-04

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// function minhaFuncao(){
+//     console.log("Testando")
+// }
+
+// minhaFuncao()
+
+// const minhaFuncaoEmVariavel = function(txt){
+//     console.log(`Imprimindo: ${txt}`)
+// }
+
+// minhaFuncaoEmVariavel("sapato")
+// minhaFuncaoEmVariavel("colher")
+// minhaFuncaoEmVariavel(5+5)
+
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = 40;
+
+// function soma(n1, n2){
+//     console.log(`A soma entre ${n1} + ${n2} é = ${n1 + n2}`)
+//     console.log(`A soma entre ${n1} + ${n2} é = ${n1 + n2}`)
+//     console.log(`A soma entre ${n1} + ${n2} é = ${n1 + n2}`)
+//     console.log(`A soma entre ${n1} + ${n2} é = ${n1 + n2}`)
+// }
+
+// soma(a,b)
+// soma(c,b)
+// soma(d,a)
+
+// function raizQuadrada(numero) {
+//     return Math.sqrt(numero);
+// }
+
+// console.log(raizQuadrada(9)); 
+
+// const testeArrow = () => {
+//     console.log("Essa é uma arrow function")
+// }
+
+// testeArrow()
+
+// const parOUImpar = (n) =>{
+//     if(n % 2 === 0){
+//         console.log(`${n} é par`)
+//         return
+//     }
+//     console.log(`${n} é impar`)
+// }
+
+// parOUImpar(2)
+
+// const lista = [1, 2, 3, 4, 5]
+
+// console.log(lista)
+// console.log(typeof lista)
+
+// console.log(lista [1])
+// console.log(lista [0])
+// console.log(lista [6])
+// console.log(lista, length)
+
+// const lista2 = ["Fusca", "variant", "Chevete", "Golf", "Opala", "Brasilia"]
+// // console.log(lista2.length)
+// console.log(lista2)
+// for(let i = 0; i < lista2.length; i++){
+//     console.log(`Elemento da lista: ${lista2[i]}`)
+// }
+
+
+
+// Escreva um array chamado de cores "azul amarelo verde vermelho e branco"
+// escreva uma estrutura de repetição laço for para retonar todos os elementos array
+
+const matBasica = ["Azul", "amarelo", "verde", "vermelho", "branco"]
+console.log(matBasica)
+for(let i = 0; i < matBasica.length; i++){
+    console.log(`Elementos da lista: ${matBasica[i]}`)
+}
+
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((n) => {
+    console.log(`O numero atual é: ${n}`)
+})
